3ae903a34b921ac7a492b655ff981895c43be79e
Ruben Oostinga <ruben.oostinga@gmail.com>
2017-08-01 16:10:00 +0200

    Add basic stream rendering

--- packages/stream-app/server.js
+++ packages/stream-app/server.js
@@ -1,12 +1,14 @@
 import Koa from 'koa';
+import React from 'react';
+import { renderToStream } from 'react-dom/node-stream';
 
 const app = new Koa();
 
 // response
 app.use(ctx => {
-  ctx.body = 'Hello Koa';
+  ctx.body = renderToStream(<h1>hello!</h1>);
 });
 
 app.listen(3000, () => {
-  console.log('Listening on http://localhost:3000')
+  console.log('Listening on http://localhost:3000');
 });
